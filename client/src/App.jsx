c8ea4d74374c05d8ca6cd0cae80a69e20de1fa86
Daniel Jankowski <djank0113@gmail.com>
2024-11-13 20:09:41 -0500

    chore: update react-router-dom to version 6.28.0 and implement basic routing in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
+import React from 'react';
 import { useState, useEffect } from 'react';
 import { Dialog } from '@headlessui/react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import getHealth from './services/test.api.js';
 import logger from '../config/logger.js';
 import FontAwesomeIcon from './components/common/Icon.jsx';
@@ -7,9 +10,21 @@ import ServerTest from './components/common/ServerTest.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <ServerTest />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<ServerTest />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
