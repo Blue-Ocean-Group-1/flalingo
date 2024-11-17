ba26708cdb7c8f4a3d5711c9bfc70ef1fa58e4c8
Robert Potter <robert.potter.741@gmail.com>
2024-11-17 17:10:18 -0500

    Implement badge object, badge logic, badge assets

---
+++ client/src/assets/Badges/badgeObject.js
@@ -0,0 +1,33 @@
+import DailyTasks from './DailyTasks.png';
+import FamilyAdvanced from './FamilyAdvanced.png';
+import FamilyBeginner from './FamilyBeginner.png';
+import FamilyProficient from './FamilyProficient.png';
+import FoodAdvanced from './FoodAdvanced.png';
+import FoodBeginner from './FoodBeginner.png';
+import FoodProficient from './FoodProficient.png';
+import HouseholdAdvanced from './HouseholdAdvanced.png';
+import HouseholdProficient from './HouseholdProficient.png';
+import HouseholdBeginner from './HouseholdBeginner.png';
+import PerfectDeck from './PerfectDeck.png';
+import TravelAdvanced from './TravelAdvanced.png';
+import TravelBeginner from './TravelBeginner.png';
+import TravelProficient from './TravelProficient.png';
+
+const badgeObject = {
+  DailyTasks,
+  FamilyAdvanced,
+  FamilyBeginner,
+  FamilyProficient,
+  FoodAdvanced,
+  FoodBeginner,
+  FoodProficient,
+  HouseholdAdvanced,
+  HouseholdProficient,
+  HouseholdBeginner,
+  PerfectDeck,
+  TravelAdvanced,
+  TravelBeginner,
+  TravelProficient,
+};
+
+export default badgeObject;
