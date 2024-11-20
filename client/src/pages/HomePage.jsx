732397030ba7f254fcd2bad19877709633390bdb
Robert Potter <robert.potter.741@gmail.com>
2024-11-19 23:15:23 -0500

    Update HomePage.jsx
    just used what I have updated but git wont let me push :(

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,43 +1,49 @@
 import axios from 'axios';
-import Navbar from '../components/Navbar';
 import useUserData from '../hooks/useUserData.jsx';
 import { useEffect, useState } from 'react';
 import { env } from '../../config/env';
 import { addTimesCompleted } from '../utils/addDeckProgress.js';
-import { findBestDisplayDecks } from '../utils/deckProgress';
-import logger from '../../config/logger.js';
+import {
+  findBestDisplayDecks,
+  findRecommendedDeck,
+} from '../utils/deckProgress';
 
 import ProgressCircle from '../components/dashboard/progressCircle';
 import DailyWord from '../components/dashboard/DailyWord';
 import MainProgress from '../components/dashboard/MainProgress';
-import RecommendedLessons from '../components/dashboard/RecommendedLessons';
+import UserReportDisplay from '../components/dashboard/UserReportDisplay';
+import DefaultPageLayout from '../components/layout/DefaultPageLayout';
 
 export default function HomePage() {
-  const [userData] = useUserData();
   const [dailyWords, setDailyWords] = useState([]);
   const [dailyMotivation, setDailyMotivation] = useState('');
+  const [userData] = useUserData();
   const [user, setUser] = useState('');
   const [displayDecks, setDisplayDecks] = useState([]);
+  const [recommendedDeck, setRecommendedDeck] = useState(null);
   const [maxPercentage, setMaxPercentage] = useState(0);
 
   // You want data? This will give you data
   useEffect(() => {
-    const getData = async () => {
-      logger.info('HomePage: user data:', userData);
-      axios.get(`${env.API_URL}/users/${userID}`).then((res) => {
-        setUser(res.data);
-        setDisplayDecks(findBestDisplayDecks(res.data));
-      });
+    setUser(userData);
+    if (userData?.progress?.length) {
+      setDisplayDecks(findBestDisplayDecks(userData));
+      setRecommendedDeck(findRecommendedDeck(userData));
+    }
+
+    const getRecommendedDeck = async () => {
+      const deck = await findRecommendedDeck(userData);
+      setRecommendedDeck(deck[0]);
     };
-    getData();
+    getRecommendedDeck();
 
     setDailyMotivation('Do it!');
     const getRandomWords = async () => {
       const response = await axios.get(
-        `${env.API_URL}/users/${userID}/dailyWords`,
+        `${env.API_URL}/users/${userData._id}/dailyWords`,
         {
           params: {
-            id: userID,
+            id: userData._id,
           },
         },
       );
@@ -59,6 +65,10 @@ export default function HomePage() {
     }
   }, [displayDecks]);
 
+  useEffect(() => {
+    console.log(recommendedDeck);
+  }, [recommendedDeck]);
+
   const flipWord = (index) => {
     let newDailyWords = [...dailyWords];
     newDailyWords[index].flipped = !newDailyWords[index].flipped;
@@ -66,54 +76,66 @@ export default function HomePage() {
   };
 
   return (
-    <div>
-      <Navbar />
-      <h1>Home Page</h1>
-      <div className="flex bg-white -mt-6">
-        <div className="p-8 w-1/2 flex flex-col justify-around items-center gap-8">
-          <div className="flex flex-col p-4 rounded-lg gap-4 bg-white w-2/4">
-            <div className="flex justify-center align-center">
-              <h3 className="text-5xl text-jet">My Daily Words</h3>
+    <DefaultPageLayout>
+      <section className="flex content-center items-center rounded-xl w-full">
+        <div className="flex bg-white mt-12 rounded-2xl w-full">
+          <div className="p-8 w-1/2 flex flex-col justify-between items-center gap-8">
+            <div className="flex flex-col p-4 rounded-lg gap-4 bg-white w-2/4">
+              <div className="flex justify-center align-center">
+                <h3 className="text-5xl text-jet">My Daily Words</h3>
+              </div>
+              {dailyWords?.length &&
+                dailyWords.map((word, index) => (
+                  <DailyWord
+                    key={word._id}
+                    word={word}
+                    index={index}
+                    flipWord={flipWord}
+                  />
+                ))}
             </div>
-            {dailyWords?.length &&
-              dailyWords.map((word, index) => (
-                <DailyWord
-                  key={word._id}
-                  word={word}
-                  index={index}
-                  flipWord={flipWord}
-                />
-              ))}
-          </div>
-          <div className="bg-transparent rounded-lg flex w-1/2 flex-col max-w-2/4 px-2">
-            <div className="flex justify-center align-center">
-              <h3 className="text-5xl text-jet">My Decks</h3>
-            </div>
-            {displayDecks?.length
-              ? displayDecks.map((deck) => (
+            <div className="bg-transparent rounded-lg flex w-1/2 flex-col max-w-2/4 px-2">
+              <div className="flex justify-center align-center">
+                <h3 className="text-5xl text-jet">My Decks</h3>
+              </div>
+              {!!displayDecks?.length &&
+                displayDecks.map((deck) => (
                   <ProgressCircle
                     key={deck._id}
                     deck={deck}
                     language={user.activeLanguages[0]}
                     maxPercentage={maxPercentage}
+                    recommended={false}
                   />
-                ))
-              : null}
-            <div className="flex justify-center align-center">
-              <button
-                onClick={addTimesCompleted}
-                className="p-2 rounded-xl bg-argentBlue text-jet w-1/3 m-2 mt-6 font-bold hover:scale-105"
-              >
-                Start A New Deck
-              </button>
+                ))}
+              {recommendedDeck?.name && (
+                <ProgressCircle
+                  deck={{
+                    ...recommendedDeck,
+                    percentage: 0,
+                    deckName: recommendedDeck.name,
+                  }}
+                  language={user.activeLanguages[0]}
+                  maxPercentage={0}
+                  recommended={true}
+                />
+              )}
+              <div className="flex justify-center align-center">
+                <button
+                  onClick={addTimesCompleted}
+                  className="p-2 rounded-xl bg-argentBlue text-jet w-1/3 m-2 mt-6 font-bold hover:scale-105"
+                >
+                  Start A New Deck
+                </button>
+              </div>
             </div>
           </div>
+          <div className="p-8 w-1/2 flex flex-col justify-around items-center gap-8 pr-20">
+            <MainProgress user={user} />
+            {user && <UserReportDisplay user={user} />}
+          </div>
         </div>
-        <div className="p-8 w-1/2 flex flex-col justify-around items-center gap-8">
-          <MainProgress user={user} />
-          <RecommendedLessons />
-        </div>
-      </div>
-    </div>
+      </section>
+    </DefaultPageLayout>
   );
 }
